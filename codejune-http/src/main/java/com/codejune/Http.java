70494bef21b5ad5e1d8cea6352f233466a028723
ZJ <1476253236@qq.com>
2022-08-23 15:47:51 +0800

    优化http

--- codejune-http/src/main/java/com/codejune/Http.java
+++ codejune-http/src/main/java/com/codejune/Http.java
@@ -48,7 +48,6 @@ public final class Http {
         // 默认请求头
         header.put("accept", "*/*");
         header.put("connection", "Keep-Alive");
-        header.put("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");
     }
 
     /**
