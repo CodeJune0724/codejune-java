bcacafdacca76c74b72e96584ca9c8ec5ae31b21
ZJ <1476253236@qq.com>
2022-11-21 16:18:10 +0800

    修复

--- codejune-common/src/main/java/com/codejune/common/DataType.java
+++ codejune-common/src/main/java/com/codejune/common/DataType.java
@@ -229,16 +229,14 @@ public enum DataType {
                 for (Field field : allFields) {
                     Object value = field.getData(object);
                     Method method = classInfo.getMethod(BeanUtil.getGetterMethodName(field.getName(), parse(field.getType())));
-                    boolean isExecuteMethod = false;
                     if (method != null) {
-                        isExecuteMethod = true;
                         try {
                             value = method.execute(object);
-                        } catch (Exception ignored) {}
+                        } catch (Exception e) {
+                            value = null;
+                        }
                     }
-                    if (!isExecuteMethod) {
-                        result.put(field.getName(), transform(value, field.getType()));
-                    }
+                    result.put(field.getName(), transform(value, field.getType()));
                 }
                 return result;
             }
