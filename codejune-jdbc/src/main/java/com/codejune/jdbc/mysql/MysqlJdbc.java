8bcb753f60c4b78794a710d1370f5a7e69ab276a
ZJ <1476253236@qq.com>
2022-12-03 10:54:02 +0800

    1

--- codejune-jdbc/src/main/java/com/codejune/jdbc/mysql/MysqlJdbc.java
+++ codejune-jdbc/src/main/java/com/codejune/jdbc/mysql/MysqlJdbc.java
@@ -48,7 +48,7 @@ public class MysqlJdbc extends SqlJdbc {
     }
 
     @Override
-    public final List<? extends MysqlDatabase> getDatabases() {
+    public final List<MysqlDatabase> getDatabases() {
         List<MysqlDatabase> result = new ArrayList<>();
         for (Map<String, Object> item : query("SHOW DATABASES")) {
             result.add(getDatabase(MapUtil.getValue(item, "Database", String.class)));
