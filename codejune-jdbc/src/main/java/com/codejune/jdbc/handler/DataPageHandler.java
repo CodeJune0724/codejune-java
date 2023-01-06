052be0c11a39a68ebbbef1023a12d755323c0fbc
ZJ <1476253236@qq.com>
2023-01-06 23:48:32 +0800

    优化数据分页

--- codejune-jdbc/src/main/java/com/codejune/jdbc/handler/DataPageHandler.java
+++ codejune-jdbc/src/main/java/com/codejune/jdbc/handler/DataPageHandler.java
@@ -1,8 +1,10 @@
 package com.codejune.jdbc.handler;
 
+import com.codejune.common.exception.InfoException;
 import com.codejune.jdbc.Query;
 import com.codejune.jdbc.Table;
 import com.codejune.jdbc.query.Filter;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,12 +14,26 @@ import java.util.Map;
  * */
 public abstract class DataPageHandler extends com.codejune.common.DataPageExecutor<Map<String, Object>> {
 
+    private final Table table;
+
+    private final Filter filter;
+
     public DataPageHandler(int size, Table table, Filter filter) {
-        super(size, (page, size1) -> table.query(new Query().setPage(page).setSize(size1).setFilter(filter)).getData());
+        super(size);
+        if (table == null) {
+            throw new InfoException("table is null");
+        }
+        this.table = table;
+        this.filter = filter;
     }
 
     public DataPageHandler(int size, Table table) {
         this(size, table, null);
+    }
+
+    @Override
+    public final List<Map<String, Object>> queryData(int page, int size) {
+        return table.query(new Query().setPage(page).setSize(size).setFilter(filter)).getData();
     }
 
 }
