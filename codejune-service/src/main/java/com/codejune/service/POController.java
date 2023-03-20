93d66609cf67b71f67ea5531632936566e7f12fb
ZJ <1476253236@qq.com>
2023-03-20 10:36:51 +0800

    1.7.2

--- codejune-service/src/main/java/com/codejune/service/POController.java
+++ codejune-service/src/main/java/com/codejune/service/POController.java
@@ -1,67 +1,31 @@
 package com.codejune.service;
 
-import com.codejune.common.exception.InfoException;
-import com.codejune.jdbc.Query;
 import com.codejune.common.ResponseResult;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-public final class POController<T extends BasePO<ID>, ID> {
+public interface POController<T extends BasePO<ID>, ID> {
 
     @PostMapping()
-    public ResponseResult save(@RequestBody(required = false) T requestBody, POService<T, ID> POService) {
-        if (requestBody != null) {
-            requestBody.setId(null);
-        }
-        return ResponseResult.returnTrue(POService.save(requestBody));
-    }
+    ResponseResult save(@RequestBody(required = false) T requestBody);
 
     @PostMapping("saveList")
-    public ResponseResult saveList(@RequestBody(required = false) List<T> requestBody, POService<T, ID> POService) {
-        if (requestBody != null) {
-            for (T t : requestBody) {
-                t.setId(null);
-            }
-        }
-        return ResponseResult.returnTrue(POService.save(requestBody));
-    }
+    ResponseResult saveList(@RequestBody(required = false) List<T> requestBody);
 
     @DeleteMapping("{id}")
-    public ResponseResult delete(@PathVariable(required = false) ID id, POService<T, ID> POService) {
-        POService.delete(id);
-        return ResponseResult.returnTrue();
-    }
+    ResponseResult delete(@PathVariable(required = false) ID id);
 
     @DeleteMapping("deleteList")
-    public ResponseResult deleteList(@RequestBody(required = false) List<ID> requestBody, POService<T, ID> POService) {
-        if (requestBody != null) {
-            for (ID id : requestBody) {
-                POService.delete(id);
-            }
-        }
-        return ResponseResult.returnTrue();
-    }
+    ResponseResult deleteList(@RequestBody(required = false) List<ID> requestBody);
 
     @PutMapping("{id}")
-    public ResponseResult update(@PathVariable(required = false) ID id, @RequestBody(required = false) T requestBody, POService<T, ID> POService) {
-        if (id == null) {
-            throw new InfoException("参数缺失");
-        }
-        if (requestBody != null) {
-            requestBody.setId(id);
-        }
-        return ResponseResult.returnTrue(POService.save(requestBody));
-    }
+    ResponseResult update(@PathVariable(required = false) ID id, @RequestBody(required = false) T requestBody);
 
     @PostMapping("query")
-    public ResponseResult query(@RequestBody(required = false) Map<String, Object> requestBody, POService<T, ID> POService) {
-        return ResponseResult.returnTrue(POService.query(Query.parse(requestBody)));
-    }
+    ResponseResult query(@RequestBody(required = false) Map<String, Object> requestBody);
 
     @GetMapping("{id}")
-    public ResponseResult getDetails(@PathVariable(required = false) ID id, POService<T, ID> POService) {
-        return ResponseResult.returnTrue(POService.getDetails(id));
-    }
+    ResponseResult getDetails(@PathVariable(required = false) ID id);
 
 }
