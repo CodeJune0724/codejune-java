8ba4e3ebcb4fe08a63ec8c70df00a9364f70ebff
ZJ <1476253236@qq.com>
2024-04-22 17:18:54 +0800

    1

--- codejune-uiAuto/src/test/java/Main.java
+++ codejune-uiAuto/src/test/java/Main.java
@@ -1,7 +1,10 @@
+import com.codejune.uiAuto.WebDriver;
+import com.codejune.uiAuto.webDriver.ChromeWebDriver;
+
 public final class Main {
 
     public static void main(String[] args) {
-
+        WebDriver webDriver= new ChromeWebDriver("");
     }
 
 }
