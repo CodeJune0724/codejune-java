0077db743780f29d4b0d989c8cc9d6a33007dd63
ZJ <1476253236@qq.com>
2024-04-22 20:38:14 +0800

    1

--- codejune-uiauto/src/test/java/Main.java
+++ codejune-uiauto/src/test/java/Main.java
@@ -4,7 +4,10 @@ import com.codejune.uiauto.webDriver.ChromeWebDriver;
 public final class Main {
 
     public static void main(String[] args) {
-        WebDriver webDriver= new ChromeWebDriver("");
+        try (WebDriver webDriver = new ChromeWebDriver("C:\\Users\\14762\\Desktop\\file\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe", true)) {
+            webDriver.open("https://www.guanwuxiaoer.com/hscode.php");
+            System.out.println();
+        }
     }
 
 }
