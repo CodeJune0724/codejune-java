2b3d6d792fb4aced2cf20fb7ceec056680b95cfb
ZJ <1476253236@qq.com>
2024-04-24 11:01:19 +0800

    1.8.143-beta

--- codejune-jdbc/build.gradle.kts
+++ codejune-jdbc/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
     api(project(":codejune-common"))
-    implementation("net.sf.ucanaccess:ucanaccess:5.0.1")
-    implementation("org.mongodb:mongodb-driver-sync:5.0.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
-    implementation("com.oracle.database.jdbc:ojdbc11:23.3.0.23.09")
-    implementation("com.mysql:mysql-connector-j:8.3.0")
+    compileOnly("net.sf.ucanaccess:ucanaccess:5.0.1")
+    compileOnly("org.mongodb:mongodb-driver-sync:5.0.0")
+    compileOnly("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
+    compileOnly("com.oracle.database.jdbc:ojdbc11:23.3.0.23.09")
+    compileOnly("com.mysql:mysql-connector-j:8.3.0")
 }
