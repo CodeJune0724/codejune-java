fe6a0d54c0d25ef2bc4152444c31c89999479a8a
ZJ <1476253236@qq.com>
2026-08-19 14:02:54 +0800

    1.11.46

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 subprojects {
     group = "com.codejune"
 
-    version = "1.11.45"
+    version = "1.11.46"
 
     apply {
         plugin("java")
---
+++ codejune-core/src/main/java/com/codejune/core/thread/SyncTaskThreadExecutor.java
@@ -0,0 +1,40 @@
+package com.codejune.core.thread;
+
+/**
+ * 异步任务执行器
+ *
+ * @author ZJ
+ * */
+public abstract class SyncTaskThreadExecutor {
+
+    private boolean status = false;
+
+    /**
+     * 任务逻辑
+     * */
+    public abstract void handler();
+
+    /**
+     * 开始运行
+     * */
+    public final synchronized void run() {
+        if (this.status) {
+            return;
+        }
+        Thread.startVirtualThread(() -> {
+            try {
+                this.handler();
+            } finally {
+                this.stop();
+            }
+        });
+    }
+
+    /**
+     * 停止
+     * */
+    public final void stop() {
+        this.status = false;
+    }
+
+}
