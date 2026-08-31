7dacfd54de991fa9bb09466cbbd6e2d4065303ea
ZJ <1476253236@qq.com>
2026-09-01 00:29:28 +0800

    1.11.50

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 subprojects {
     group = "com.codejune"
 
-    version = "1.11.49"
+    version = "1.11.50"
 
     apply {
         plugin("java")
--- codejune-javafx/src/main/java/com/codejune/javafx/component/Input.java
+++ codejune-javafx/src/main/java/com/codejune/javafx/component/Input.java
@@ -13,6 +13,7 @@ public final class Input extends BaseComponent {
     public Input(String value) {
         this.setValue(value);
         this.getStyle().addSheet(getClass().getResource("/javafx/style/input.css")).addClass("input");
+        this.getStyle().setMinHeight(30);
     }
 
     public Input() {
